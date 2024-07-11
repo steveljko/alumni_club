905ede6f26e08114c79fcf45b7e28fc64cb3ba42
steveljko <steveljko@proton.me>
2024-07-11 11:41:04 +0200

    add user details migration and model

---
+++ app/Models/UserDetails.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class UserDetails extends Model
+{
+    use HasFactory;
+
+    /** @var array */
+    public $fillable = [
+        'date_of_birth',
+        'gender',
+        'email',
+        'email_visible',
+        'phone_number',
+        'phone_number_visible',
+        'uni_start_year',
+        'uni_finish_year',
+        'bio',
+    ];
+
+    /** @return \Illuminate\Database\Eloquent\Relations\HasMany */
+    public function user(): HasOne
+    {
+        return $this->hasMany(User::class);
+    }
+}
---
+++ database/migrations/2024_07_10_124159_create_user_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('date_of_birth');
+            $table->string('gender');
+            $table->string('email');
+            $table->boolean('email_visible')->default(false);
+            $table->string('phone_number');
+            $table->boolean('phone_number_visible')->default(false);
+            $table->string('uni_start_year');
+            $table->string('uni_finish_year');
+            $table->text('bio');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
