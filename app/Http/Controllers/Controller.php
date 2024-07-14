fe582ac2c377a52da5c742899564944316696660
steveljko <steveljko@proton.me>
2024-07-14 17:27:22 +0200

    add sanctum security scheme to swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,22 +2,32 @@
 
 namespace App\Http\Controllers;
 
+// TODO: Try to make sanctum cookie auth work in Swagger UI
+
 /**
  * @OA\Info(
  *  title="Alumni Club API",
  *  version="0.1",
  *
  *  @OA\Contact(
- *      email="steveljko@proton.me"
+ *    email="steveljko@proton.me"
  *  ),
+ *
+ *  @OA\SecurityScheme(
+ *    securityScheme="sanctum",
+ *    type="apiKey",
+ *    in="cookie",
+ *    name="XSRF-TOKEN",
+ *    description="Use the XSRF-TOKEN cookie for CSRF protection"
  *  ),
  *
  *  @OA\Server(
- *      description="Test",
- *      url="https://localhost:8000/api/"
- *  ),
+ *    description="Test",
+ *    url="http://localhost:8000/api/"
+ *  )
+ * )
  */
 abstract class Controller
 {
-    //
+  //
 }
