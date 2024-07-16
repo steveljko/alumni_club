83ad6548dcb1973d2a7357c93e6a0523ba50b1e9
steveljko <steveljko@proton.me>
2024-07-16 14:55:35 +0200

    change unauthenticated api message

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,5 +24,11 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+      // Change rendering output for AuthenticationException
+      $exceptions->render(function (AuthenticationException $e, Request $request) {
+        return new JsonResponse([
+          'success' => false,
+          'message' => 'You are unautheticated for exectuing this request.',
+        ], Response::HTTP_UNAUTHORIZED);
+      });
     })->create();
