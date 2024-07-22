fb802cdb534d59b42e3d1a96796fa8d48fae0e30
steveljko <steveljko@proton.me>
2024-07-22 20:49:11 +0200

    move 'email_visible' field from user details to user migration

--- database/migrations/2024_07_10_124159_create_user_details_table.php
+++ database/migrations/2024_07_10_124159_create_user_details_table.php
@@ -6,32 +6,31 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('user_details', function (Blueprint $table) {
-            $table->id();
-            $table->string('date_of_birth')->nullable();
-            $table->string('gender')->nullable();
-            $table->string('email_visible')->default(false);
-            $table->string('phone_number')->nullable();
-            $table->boolean('phone_number_visible')->default(false);
-            $table->string('uni_start_year')->nullable();
-            $table->string('uni_finish_year')->nullable();
-            $table->text('bio')->nullable();
-            $table->foreignId('user_id')->constrained();
-            $table->integer('changed')->default(false);
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('user_details', function (Blueprint $table) {
+      $table->id();
+      $table->string('date_of_birth')->nullable();
+      $table->string('gender')->nullable();
+      $table->string('phone_number')->nullable();
+      $table->boolean('phone_number_visible')->default(false);
+      $table->string('uni_start_year')->nullable();
+      $table->string('uni_finish_year')->nullable();
+      $table->text('bio')->nullable();
+      $table->foreignId('user_id')->constrained();
+      $table->integer('changed')->default(false);
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('user_details');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('user_details');
+  }
 };
