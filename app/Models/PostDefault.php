03ddfe0839bbb9f3e5f2f7505987352b5940d52e
steveljko <steveljko@proton.me>
2024-08-05 11:44:45 +0200

    feat(posts): add default type

---
+++ app/Models/PostDefault.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class PostDefault extends Model
+{
+    use HasFactory;
+
+    public function data(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
---
+++ database/migrations/2024_08_05_094017_create_post_defaults_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_defaults', function (Blueprint $table) {
+            $table->id();
+            $table->text('body');
+            $table->foreignId('post_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_defaults');
+    }
+};
