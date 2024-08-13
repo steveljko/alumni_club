e1aada6c0c0e7875046cb16d8bf175478b15b7cb
steveljko <steveljko@proton.me>
2024-08-13 14:36:30 +0200

    feat: add http messages with localization support

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,11 +7,30 @@ use Illuminate\Http\JsonResponse;
 
 class Controller
 {
+    private ?string $model = null;
+
+    private ?string $type = null;
+
+    public function __construct()
+    {
+        $ref = new \ReflectionClass($this);
+        $class = $ref->getShortName();
+
+        preg_match('/^[A-Z][a-z]*/', $class, $matches);
+
+        if (count($matches) == 3) {
+            $this->model = strtolower($matches[1]);
+            $this->type = strtolower($matches[0]);
+        } elseif (count($matches) == 4) {
+            $this->model = strtolower($matches[2]);
+            $this->type = strtolower($matches[0]);
+        }
+    }
+
     /**
      * Send success JSON message
      *
      * @var string
-     * @var
      * @var int
      */
     public function sendResponse(
@@ -45,5 +64,39 @@ class Controller
             'success' => false,
             'message' => $message,
         ], $status);
+    }
+
+    /* Methods for sending localized messages in the controller */
+    protected function sendOk(): JsonResponse
+    {
+        return $this->sendResponse(
+            message: __("additional.{$this->getModel()}s.successful_{$this->getType()}")
+        );
+    }
+
+    protected function sendUnauthorized(): JsonResponse
+    {
+        return $this->sendFailResponse(
+            message: __("additional.{$this->getModel()}s.unauthorized"),
+            status: Response::HTTP_UNAUTHORIZED,
+        );
+    }
+
+    protected function sendForbidden(): JsonResponse
+    {
+        return $this->sendFailResponse(
+            message: __("additional.{$this->getModel()}s.{$this->getType()}_failed"),
+        );
+    }
+
+    /* Getters */
+    protected function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    protected function getType(): ?string
+    {
+        return $this->type;
     }
 }
