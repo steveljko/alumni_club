0e8321d0e43a193ec3b020bc3306eacbd3d9fc8d
steveljko <steveljko@proton.me>
2024-08-15 19:00:36 +0200

    feat(models): add 'Images' model with migration

---
+++ database/migrations/2024_08_15_115929_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('path');
+            $table->string('type')->default('general');
+
+            $table->nullableMorphs('model');
+
+            $table
+                ->foreignId('uploaded_by')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('images');
+    }
+};
