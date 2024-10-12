1857a38e2144bfe6373a9151182fc8591827e0b2
steveljko <steveljko@proton.me>
2024-10-13 01:00:49 +0200

    chore: change Inter to default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Inter', 'sans-serif'],
+            }
+        },
+    },
+    plugins: [],
 }
 
