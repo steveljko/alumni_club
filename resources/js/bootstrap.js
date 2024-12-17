b6778cead17b56152b61ba0ba276c3909923960b
steveljko <steveljko@proton.me>
2024-12-17 22:41:27 +0100

    Add validation error handling for input

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,14 @@
 import htmx from "htmx.org";
 
 window.htmx = htmx;
+
+document.addEventListener('htmx:responseError', function (event) {
+    const errors = JSON.parse(event.detail.xhr.response).errors;
+
+    for (const [field, messages] of Object.entries(errors)) {
+        const container = document.querySelector(`#${field}-validation-message`);
+        container.classList.remove('hidden');
+        container.innerHTML = messages[0];
+    }
+});
+
