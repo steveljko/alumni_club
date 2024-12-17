26294c629f372854e27a754dda220dd53248fc91
steveljko <steveljko@proton.me>
2024-12-17 23:40:37 +0100

    Set 'initial_password_changed_at' field on Users model to cast as datetime

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'initial_password_changed_at' => 'datetime',
         ];
     }
 
