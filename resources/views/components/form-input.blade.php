49d34c4803f4143bdb2691425ebd8ce23ac94a4f
steveljko <steveljko@proton.me>
2024-12-17 22:23:57 +0100

    Add Form Input component

---
+++ resources/views/components/form-input.blade.php
@@ -0,0 +1,11 @@
+<div class="mb-3">
+    <label for="{{ $name }}"
+        class="mb-2 block text-gray-600">{{ $label }}</label>
+    <input type="{{ $type }}"
+        name="{{ $name }}"
+        id="{{ $name }}"
+        class="block w-full rounded-md border border-gray-300 px-3 py-2 focus:border-blue-300 focus:outline-none"
+        hx-on:keydown="document.querySelector('#{{ $name }}-validation-message').classList.add('hidden')">
+    <span id="{{ $name }}-validation-message"
+        class="mt-2 block hidden text-sm text-red-500"></span>
+</div>
