d2343d134e8a0e3525ff648d004f35db5d8e696c
steveljko <steveljko@proton.me>
2024-12-25 21:23:01 +0100

    Refactor toast component to display session messages

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,3 +1,7 @@
 <div id="toast"
-    class="fixed bottom-8 left-1/2 z-10 hidden min-w-[250px] -translate-x-1/2 transform rounded-md bg-gray-800 p-4 text-center text-white">
+    role="status"
+    class="{{ session('toast') ? 'block' : 'hidden' }} fixed bottom-8 left-1/2 z-10 min-w-[250px] -translate-x-1/2 transform rounded-md bg-gray-800 p-4 text-center text-white">
+    @if (session('toast'))
+        {{ session('toast') }}
+    @endif
 </div>
