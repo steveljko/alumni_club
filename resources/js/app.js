c6e0d1b67b2ef14b0e26b203f385538fa97dff8d
steveljko <steveljko@proton.me>
2024-12-27 16:36:34 +0100

    Add Form Textarea component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,25 @@
 import './htmx';
 import toast from './toast';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const textarea = document.querySelector("textarea");
+
+    if (textarea) {
+        const limit = parseInt(textarea.getAttribute('data-limit'), 10);
+        const limitSpan = document.getElementById("current-letter-count");
+        const label = document.querySelector(`label[for=${textarea.getAttribute('name')}]`);
+        const validationMessage = document.getElementById(`${textarea.getAttribute('name')}-validation-message`);
+
+        function updateLetterCount() {
+            const currLenght = textarea.value.length;
+            limitSpan.textContent = currLenght;
+            validationMessage.textContent = currLenght > limit
+                ? `${label.innerHTML} limit exceeded by ${currLenght - limit} characters.`
+                : '';
+            validationMessage.classList.toggle('hidden', currLenght <= limit);
+        };
+
+        textarea.addEventListener('input', updateLetterCount);
+        updateLetterCount();
+    }
+});
---
+++ resources/views/components/form-textarea.blade.php
@@ -0,0 +1,17 @@
+<div class="mb-3 w-full">
+    <div class="flex justify-between">
+        <label for="{{ $name }}"
+            class="mb-2 block text-gray-600">{{ $label }}</label>
+        @if ($limit)
+            <span class="block text-sm text-gray-500"><span id="current-letter-count">0</span> / {{ $limit }}</span>
+        @endif
+    </div>
+    <textarea id="{{ $name }}"
+        data-limit="{{ $limit }}"
+        name="{{ $name }}"
+        class="block w-full rounded-md border border-gray-300 px-3 py-2 focus:border-blue-300 focus:outline-none"
+        hx-on:keydown="document.querySelector('#{{ $name }}-validation-message').classList.add('hidden')">
+    </textarea>
+    <span id="{{ $name }}-validation-message"
+        class="mt-2 block hidden text-sm text-red-500"></span>
+</div>
