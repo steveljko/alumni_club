9e2ed016056a0e66f35ce1375a0d9f1f8cf28ea2
steveljko <steveljko@proton.me>
2024-12-31 18:55:07 +0100

    Add modal support with htmx swap events

--- resources/js/htmx.js
+++ resources/js/htmx.js
@@ -6,6 +6,21 @@ window.htmx = htmx;
 htmx.on("toast", (e) => toast.message(e.detail.value));
 htmx.on("toast-after-redirect", (e) => toast.setDelayedMessage(e.detail.value));
 
+htmx.on("htmx:afterSwap", (e) => {
+  if (e.detail.target.id == "modal") {
+        const modal = document.getElementById("modal");
+        modal.classList.toggle('hidden');
+  }
+});
+
+htmx.on("htmx:beforeSwap", (e) => {
+    if (e.detail.target.id == "modal" && !e.detail.xhr.response) {
+        const modal = document.getElementById("modal");
+        modal.classList.toggle('hidden');
+        e.detail.shouldSwap = false;
+    }
+});
+
 document.addEventListener('htmx:responseError', function (event) {
     const errors = JSON.parse(event.detail.xhr.response).errors;
 
