5b8f00064fdd46e5d185f2ec66e2b71e9e43ba36
steveljko <steveljko@proton.me>
2025-01-02 17:26:54 +0100

    Add HTMX response methods to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\HtmxResponse;
+use Illuminate\Http\Response;
+
 abstract class Controller
 {
-    //
+    /**
+     * Redirect using HTMX
+     */
+    public function redirect(string $route): Response
+    {
+        return (new HtmxResponse)
+            ->redirectTo(routeName: $route)
+            ->send();
+    }
+
+    /**
+     * Send toast message using HTMX
+     */
+    public function toast(string $message): Response
+    {
+        return (new HtmxResponse)
+            ->toast(message: $message)
+            ->send();
+    }
+
+    /**
+     * Send toast message along with redirect request using HTMX
+     */
+    public function redirectWithToast(string $route, string $message): Response
+    {
+        return (new HtmxResponse)
+            ->redirectTo(routeName: $route)
+            ->toast(message: $message)
+            ->send();
+    }
 }
