96fd0e0985f474a11bf1b8a4807be2349a28a080
steveljko <steveljko@proton.me>
2025-01-07 09:51:49 +0100

    Add countdown line animation and update toast style

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,7 +1,10 @@
 <div id="toast"
     role="status"
-    class="{{ session('toast') ? 'block' : 'hidden' }} fixed bottom-8 left-1/2 z-10 min-w-[250px] -translate-x-1/2 transform rounded-md bg-gray-800 p-4 text-center text-white">
-    @if (session('toast'))
-        {{ session('toast') }}
-    @endif
+    class="{{ session('toast') ? 'block' : 'hidden' }} fixed bottom-8 left-1/2 z-10 min-w-[250px] -translate-x-1/2 transform rounded-md bg-white text-center text-blue-50 shadow">
+    <div class="py-3 text-gray-700">
+        @if (session('toast'))
+            {{ session('toast') }}
+        @endif
+    </div>
+    <div class="animate-shrink h-[4px] rounded-b-md bg-blue-300"></div>
 </div>
