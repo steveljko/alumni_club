5024e2e38c66a21f74a4c5730e4234d8cabc8e39
steveljko <steveljko@proton.me>
2025-01-15 12:16:52 +0100

    Disable autocomplete on input component

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -6,7 +6,8 @@
         id="{{ $name }}"
         value="{{ $value }}"
         class="block w-full rounded-md border border-gray-300 px-3 py-2 focus:border-blue-300 focus:outline-none"
-        hx-on:keydown="document.querySelector('#{{ $name }}-validation-message').classList.add('hidden')">
+        hx-on:keydown="document.querySelector('#{{ $name }}-validation-message').classList.add('hidden')"
+        autocomplete="off">
     <span id="{{ $name }}-validation-message"
         class="mt-2 block hidden text-sm text-red-500"></span>
 </div>
