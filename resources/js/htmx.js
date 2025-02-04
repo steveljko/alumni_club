2da75000bc0c83b26a7fcff55978e733e1800869
steveljko <steveljko@proton.me>
2025-02-04 21:06:39 +0100

    Refactor htmx to support new modal API

--- resources/js/htmx.js
+++ resources/js/htmx.js
@@ -1,5 +1,6 @@
 import { setupPostboxTextarea, setupTextboxCharLimit } from './app.js';
-import htmx from "htmx.org";
+import htmx from 'htmx.org';
+import modal from './modal';
 import toast from './toast';
 
 window.htmx = htmx;
@@ -7,46 +8,21 @@ window.htmx = htmx;
 htmx.on("toast", (e) => toast.message(e.detail.value));
 htmx.on("toast-after-redirect", (e) => toast.setDelayedMessage(e.detail.value));
 
-function loadModalListeners(modal) {
-    const modalContent = modal.querySelector('#modal-content');
-    const modalClose = modal.querySelector('[data-hide-modal]');
-
-    if (modalContent) {
-        modalContent.focus();
-    }
-
-    function closeModal() {
-        modal.innerHTML = '';
-        modal.classList.add('hidden');
-    }
-
-    if (modal) {
-        modal.addEventListener('keydown', (event) => {
-            if (event.key == 'Escape') closeModal();
-        });
-
-        modalClose.addEventListener('click', (event) => { closeModal() });
-    }
-}
-
 htmx.onLoad(() => {
     setupPostboxTextarea();
     setupTextboxCharLimit();
 });
 
 htmx.on('htmx:afterSwap', (e) => {
-  if (e.detail.target.id == 'modal') {
-        const modal = document.getElementById('modal');
-        modal.classList.remove('hidden');
-
-        loadModalListeners(modal);
+  if (e.detail.target.id == 'dialog') {
+        modal.show();
   }
 });
 
 htmx.on('htmx:beforeSwap', (e) => {
-    if (e.detail.target.id == 'modal' && !e.detail.xhr.response) {
-        const modal = document.getElementById('modal');
-        modal.classList.add('hidden');
+    if (e.detail.target.id == 'dialog' && !e.detail.xhr.response) {
+        modal.hide();
+
         e.detail.shouldSwap = false;
     }
 });
