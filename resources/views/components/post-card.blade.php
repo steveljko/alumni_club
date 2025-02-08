dbdcce3870e9a1f769f307e7d8c3cc61930b6e3b
steveljko <steveljko@proton.me>
2025-02-08 14:53:43 +0100

    Refactor safe redirection to modal

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,14 +1,16 @@
-  <section class="w-full rounded-lg bg-white px-4 py-5 shadow"
-      id="post-{{ $post->id }}">
+  <section class="w-full rounded-lg bg-white px-4 py-5 shadow" id="post-{{ $post->id }}">
       <div class="{{ !$post->isEventOrJob() ? 'items-start' : 'items-end relative' }} flex justify-between">
           @if ($post->isEventOrJob())
               <span
-                  class="absolute right-[-1rem] top-[-1.25rem] block rounded-bl-lg rounded-tr-lg bg-[#A3C1DA] bg-blue-400 px-3 py-1.5 text-sm font-semibold uppercase tracking-[0.08em] text-[#003366]">{{ $post->type }}</span>
+                  class="absolute right-[-1rem] top-[-1.25rem] block rounded-bl-md rounded-tr-lg bg-navyblue-500 px-3 py-1.5 text-sm font-semibold uppercase tracking-[0.08em] text-white"
+              >{{ $post->type }}</span>
           @endif
           <div class="flex-start flex items-center">
-              <img class="mr-3 h-10 w-10 rounded-full"
+              <img
+                  class="mr-3 h-10 w-10 rounded-full"
                   src="{{ asset('storage/images/' . $post->user->avatar) }}"
-                  alt="User Avatar">
+                  alt="User Avatar"
+              >
               <a href="{{ route('profile', $post->user) }}"
                   class="text-base font-medium text-gray-950 hover:text-blue-600 hover:underline">{{ $post->user->name }}</a>
           </div>
@@ -25,16 +27,18 @@
                   <div class="block space-y-2 md:flex md:space-x-2 md:space-y-0">
                       <span class="block rounded-full border border-gray-200 px-1.5 py-0.75 text-xs font-medium">
                           {{ $post->event->address }}, {{ $post->event->city }}</span>
-                      <x-badge-date :start="$post->event->start_time"
-                          :end="$post->event->end_time" />
-                      <x-badge-time :start="$post->event->start_time"
-                          :end="$post->event->end_time" />
+                      <x-badge-date :start="$post->event->start_time" :end="$post->event->end_time" />
+                      <x-badge-time :start="$post->event->start_time" :end="$post->event->end_time" />
                   </div>
               </div>
               <p class="text-sm leading-8 text-gray-800">{{ $post->event->description }}</p>
               <div class="flex w-full justify-end">
-                  <a href="{{ route('redirect', ['url' => $post->event->url()]) }}"
-                      class="text-sm font-semibold uppercase tracking-[0.03em] text-blue-600 hover:underline">Attend Event</a>
+                  <a
+                      hx-get="{{ route('redirect', ['url' => $post->event->url()]) }}"
+                      hx-target="#dialog"
+                      hx-swap="innerHTML"
+                      class="cursor-pointer text-sm font-semibold uppercase tracking-[0.03em] text-navyblue-500 hover:underline"
+                  >Attend Event</a>
               </div>
           </div>
       @elseif ($post->isJob())
@@ -49,50 +53,61 @@
                       <span class="block rounded-full border border-gray-200 px-1.5 py-0.75 text-xs font-medium">
                           {{ $post->job->company_address }}, {{ $post->job->company_city }}
                       </span>
-                      <x-badge-date :start="$post->job->start_time"
-                          :end="$post->job->start_time" />
-                      <x-badge-time :start="$post->job->end_time"
-                          :end="$post->job->end_time" />
+                      <x-badge-date :start="$post->job->start_time" :end="$post->job->start_time" />
+                      <x-badge-time :start="$post->job->end_time" :end="$post->job->end_time" />
                   </div>
               </div>
               <p class="text-sm leading-8 text-gray-800">{{ $post->job->description }}</p>
               <div class="flex w-full justify-end">
-                  <a href="{{ route('redirect', ['url' => $post->job->url()]) }}"
-                      class="text-sm font-semibold uppercase tracking-[0.03em] text-blue-600 hover:underline">Apply for Job</a>
+                  <a
+                      hx-get="{{ route('redirect', ['url' => $post->job->url()]) }}"
+                      hx-target="#dialog"
+                      hx-swap="innerHTML"
+                      class="cursor-pointer text-sm font-semibold uppercase tracking-[0.03em] text-navyblue-500 hover:underline"
+                  >Apply for Job</a>
               </div>
           </div>
       @endif
       <div hx-target="this">
           <div class="flex items-center border-t border-gray-100 pt-4">
               <div class="mr-4 flex items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg"
+                  <svg
+                      xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke-width="1.5"
                       stroke="currentColor"
-                      class="mr-2 size-[24px]">
-                      <path stroke-linecap="round"
+                      class="mr-2 size-[24px]"
+                  >
+                      <path
+                          stroke-linecap="round"
                           stroke-linejoin="round"
-                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                      />
                   </svg>
                   <span class="text-sm">123</span>
               </div>
-              <a hx-get="{{ route('post.comment', $post) }}"
+              <a
+                  hx-get="{{ route('post.comment', $post) }}"
                   hx-trigger="click"
                   hx-swap="outerHTML"
-                  class="flex cursor-pointer items-center">
-                  <svg width="24"
+                  class="flex cursor-pointer items-center"
+              >
+                  <svg
+                      width="24"
                       height="24"
                       fill="none"
                       viewBox="0 0 24 24"
                       class="mr-2"
-                      xmlns="http://www.w3.org/2000/svg">
+                      xmlns="http://www.w3.org/2000/svg"
+                  >
                       <path
                           d="M2.25 12.76C2.25 14.36 3.373 15.754 4.957 15.987C6.044 16.147 7.142 16.27 8.25 16.356V21L12.326 16.924C12.6024 16.6493 12.9735 16.4909 13.363 16.481C15.2644 16.4284 17.161 16.2634 19.043 15.987C20.627 15.754 21.75 14.361 21.75 12.759V6.741C21.75 5.139 20.627 3.746 19.043 3.513C16.711 3.17072 14.357 2.99926 12 3C9.608 3 7.256 3.175 4.957 3.513C3.373 3.746 2.25 5.14 2.25 6.741V12.759V12.76Z"
                           stroke="#828282"
                           stroke-width="1.5"
                           stroke-linecap="round"
-                          stroke-linejoin="round" />
+                          stroke-linejoin="round"
+                      />
                   </svg>
                   <span class="text-sm">{{ $post->comments_count }}</span>
               </a>
--- resources/views/resources/redirect.blade.php
+++ resources/views/resources/redirect.blade.php
@@ -1,20 +1,11 @@
-@extends('layouts.default')
-
-@section('content')
-    <div class="flex h-screen w-full items-center justify-center">
-        <div class="w-1/3 space-y-6 rounded-lg bg-white px-4 py-6 shadow">
-            <div class="text-center">
-                <p class="mb-2 break-all text-gray-700">You are being redirected to <strong>{{ $url }}</strong>.</p>
-                <p class="text-sm text-gray-500">Please click <i>'Proceed to Url'</i> to continue or <i>'Back to Previous Page'</i> to return to the
-                    previous
-                    page.</p>
-            </div>
-            <div class="flex items-center justify-center space-x-4">
-                <a href="{{ url()->previous() }}"
-                    class="rounded bg-gray-100 px-3 py-1 text-gray-700">Back to Previous Page</a>
-                <a href="{{ $url }}"
-                    class="rounded bg-blue-500 px-3 py-1 text-white">Proceed to Url</a>
-            </div>
-        </div>
-    </div>
-@endsection
+<div id="modal-content">
+    <x-modal.header>You are leaving app</x-modal.header>
+    <x-modal.body>
+        <p class="mb-2 break-all text-gray-700">You are being redirected to <strong>{{ $url }}</strong>.</p>
+        <p class="text-sm text-gray-500">Please click <i>'Continue'</i> to continue or <i>'Stay on App'</i> to cancel redirection.</p>
+    </x-modal.body>
+    <x-modal.footer>
+        <x-modal.button variant="secondary" data-hide-modal="true">Stay on App</x-modal.button>
+        <x-modal.button href="{{ $url }}">Continue</x-modal.button>
+    </x-modal.footer>
+</div>
